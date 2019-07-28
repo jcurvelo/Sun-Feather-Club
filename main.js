51fffd805b90c618e78f2725e28dabb11839995e
Jamael17 <juancurvelo99@gmail.com>
2019-07-28 09:01:59 -0430

    Changes

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const imgAbout = document.getElementById('img-about');
+const watchCalculator = document.getElementById('watch-calculator');
+let calDisplay = document.getElementById('calculator');
+watchCalculator.addEventListener('click',()=>{
+    
+    if(calDisplay.style.display == 'block'){
+        calDisplay.style.display = 'none';
+    }else{
+        calDisplay.style.display = 'block';
+    }
+})
+
 let imgId = 0;
 setInterval(()=>{
     imgAbout.style.backgroundImage = `url('./img/esport-team${imgId}.jpg')`;
